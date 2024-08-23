a8dd444151570ee8f74aa644e6aa89210824231a
John Alex Ladra <johnalexladra@gmail.com>
2024-08-23 19:20:21 +0800

    feat: Added initial axiosInstance request and response

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,9 +3,35 @@ import axios from 'axios';
 // Create an Axios instance
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL, 
+  withCredentials: true,
+  timeout: 1000,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+// Add request and response interceptors for handling tokens
+axiosInstance.interceptors.request.use(
+  config => {
+    // TODO: Get the token from the store
+    const token = localStorage.getItem('authToken'); 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    // Handle token expiration, etc.
+    if (error.response && error.response.status === 401) {
+      // For example, you can dispatch a logout action here
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
